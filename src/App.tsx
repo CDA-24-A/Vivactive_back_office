2f7540947e74741ddb00f85045eb0d7a382c050a
Benjamin <benjamin.langlois@devinfluence.fr>
2025-03-30 17:56:09 +0200

    Feat: Ajout du header général

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import Button from "@mui/material/Button";
 import { Route, Routes } from "react-router-dom";
 import Test from "./pages/test";
 import Index from "./pages";
 import Erreur404 from "./pages/erreur404";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [headerHeight, setHeaderHeight] = useState(0);
+
+  useEffect(() => {
+    setHeaderHeight(document.querySelector("header")?.clientHeight || 0);
+  }, []);
 
   return (
-    <>
+    <div className="app" style={{ height: `calc(100vh - ${headerHeight}px)` }}>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/test" element={<Test />} />
         <Route path="*" element={<Erreur404 />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/header.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, InputBase, IconButton, Box, Avatar, Menu, MenuItem } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar position="static" color="default" elevation={1} sx={{ width: "100%", top: 0 }}>
+      <Toolbar>
+        <Box sx={{ flexGrow: 1, margin: "20px 0px" }}>
+          <img src="/logo.png" alt="Vivactive Logo" style={{ height: 45 }} />
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center", bgcolor: "rgba(0,0,0,0.05)", borderRadius: 1, px: 2 }}>
+          <SearchIcon />
+          <InputBase placeholder="Rechercher" sx={{ ml: 1, flex: 1 }} inputProps={{ "aria-label": "search" }} />
+        </Box>
+        <IconButton size="large" aria-label="Voir les notifications" color="inherit" sx={{ ml: 2 }}>
+          <NotificationsIcon />
+        </IconButton>
+        <IconButton onClick={handleMenu} color="inherit">
+          <Avatar alt="Photo de profil" src="/path/to/avatar.jpg" />
+        </IconButton>
+        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+          <MenuItem onClick={handleClose}>Profil</MenuItem>
+          <MenuItem onClick={handleClose}>Déconnexion</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
